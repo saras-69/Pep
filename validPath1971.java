7b9f035c42957fdf053ba7e02151c3061b2d6a45
saras-69 <115482884+saras-69@users.noreply.github.com>
2025-07-03 15:53:31 +0530

    commit

--- validPath1971.java
+++ validPath1971.java
@@ -37,3 +37,26 @@ class Solution {
         return false;
     }
 }
+
+public class validPath1971 {
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+
+        // Example 1: n = 3, edges = [[0,1],[1,2],[2,0]], source = 0, destination = 2
+        int n1 = 3;
+        int[][] edges1 = { { 0, 1 }, { 1, 2 }, { 2, 0 } };
+        int source1 = 0;
+        int destination1 = 2;
+        System.out.println("Example 1: " + solution.validPath1971(n1, edges1, source1, destination1)); // Should return
+                                                                                                       // true
+
+        // Example 2: n = 6, edges = [[0,1],[0,2],[3,5],[5,4],[4,3]], source = 0,
+        // destination = 5
+        int n2 = 6;
+        int[][] edges2 = { { 0, 1 }, { 0, 2 }, { 3, 5 }, { 5, 4 }, { 4, 3 } };
+        int source2 = 0;
+        int destination2 = 5;
+        System.out.println("Example 2: " + solution.validPath1971(n2, edges2, source2, destination2)); // Should return
+                                                                                                       // false
+    }
+}
