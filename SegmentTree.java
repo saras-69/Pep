6fbee92b996f40335c0f530438f5e95db9708c64
saras-69 <115482884+saras-69@users.noreply.github.com>
2025-07-07 16:38:32 +0530

    commit

--- SegmentTree.java
+++ SegmentTree.java
@@ -100,6 +100,8 @@ public class SegmentTree {
             arr[i] = sc.nextInt();
         SegmentTree segmentTree = new SegmentTree(arr);
         segmentTree.display();
+        System.out.println("Sum b/w range 2,6");
+        System.out.println(segmentTree.rangeQuery(2, 6));
 
     }
 
