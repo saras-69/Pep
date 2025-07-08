7aa0da04840ae263dc70aa8bd6baf31af2f6b6ab
saras-69 <115482884+saras-69@users.noreply.github.com>
2025-07-08 16:38:13 +0530

    commit

---
+++ FenwickTree.java
@@ -0,0 +1,34 @@
+public class FenwickTree {
+    int arr[];
+    int bits[];
+    int N;
+
+    FenwickTree(int N) {
+        this.N = N;
+        this.arr = new int[N];
+        this.bits = new int[N + 1];
+    }
+
+    public void update(int idx, int elt) {
+        while (idx <= 0) {
+            bits[idx] += elt;
+            idx += idx & -idx;
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] arr = { 10, 20, 30, 40, 50, 60, 70, 80 };
+        int N = arr.length;
+        FenwickTree fenwickTree = new FenwickTree(N);
+        for (int i = 1; i <= N; i++) {
+            fenwickTree.update(i, arr[i - 1]);
+
+        }
+        for (int bit : fenwickTree.bits) {
+            System.out.println(bit + " ");
+        }
+        System.out.println();
+
+    }
+
+}
