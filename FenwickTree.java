c066d27593d566edc27dc144ae82cd7661654247
saras-69 <115482884+saras-69@users.noreply.github.com>
2025-07-09 14:14:51 +0530

    commit

--- FenwickTree.java
+++ FenwickTree.java
@@ -16,6 +16,20 @@ public class FenwickTree {
         }
     }
 
+    public int range(int idx) {
+        int sum = 0;
+        while (idx > 0) {
+            sum += bits[idx];
+            idx -= idx & -idx;
+
+        }
+        return sum;
+    }
+
+    public int rangeQuery(int i, int j) {
+        return range(j) - range(i - 1);
+    }
+
     public static void main(String[] args) {
         int[] arr = { 10, 20, 30, 40, 50, 60, 70, 80 };
         int N = arr.length;
@@ -28,6 +42,7 @@ public class FenwickTree {
             System.out.println(bit + " ");
         }
         System.out.println();
+        System.out.println(fenwickTree.range(3));
 
     }
 
