d1b96eaa2b041acd39545d9132e629a935f4ac63
saras-69 <115482884+saras-69@users.noreply.github.com>
2025-07-09 14:59:16 +0530

    commit

--- SegmentTree.java
+++ SegmentTree.java
@@ -5,13 +5,13 @@ public class SegmentTree {
 
     static class Node {
         int data;
-        int startInterval, endInterval;
+        int startInterval;
+        int endInterval;
         Node left, right;
 
-        Node(int startInterval, int endInterval) {
-            this.startInterval = startInterval;
-            this.endInterval = endInterval;
-
+        Node(int startIndex, int endIndex) {
+            this.startInterval = startIndex;
+            this.endInterval = endIndex;
         }
     }
 
@@ -19,13 +19,13 @@ public class SegmentTree {
         this.root = buildTree(arr, 0, arr.length - 1);
     }
 
+    // T(N) = O(N)
     public Node buildTree(int[] arr, int start, int end) {
         if (start == end) {
             Node leaf = new Node(start, end);
             leaf.data = arr[start];
             return leaf;
         }
-
         Node node = new Node(start, end);
         int mid = (start + end) / 2;
         node.left = buildTree(arr, start, mid);
@@ -34,65 +34,66 @@ public class SegmentTree {
         return node;
     }
 
-    // T(N)=O(Log N)
-    public int rangeQuery(int sIdx, int eIdx) {
-        return rangeQuery(root, sIdx, eIdx);
+    // T(N) = O(log N)
+    public int rangeQuery(int startInterval, int endInterval) {
+        return rangeQuery(root, startInterval, endInterval);
     }
 
-    private int rangeQuery(Node root, int sIdx, int eIdx) {
-        if (root.startInterval > eIdx || root.endInterval < sIdx) {
+    public int rangeQuery(Node root, int sIdx, int eIdx) {
+        if (root == null)
             return 0;
-        }
-
-        if (root.startInterval >= sIdx && root.endInterval <= eIdx) {
+        else if (sIdx <= root.startInterval && eIdx >= root.endInterval)
             return root.data;
-        }
-
-        int leftAns = rangeQuery(root.left, sIdx, eIdx);
-        int rightAns = rangeQuery(root.right, sIdx, eIdx);
-
-        return leftAns + rightAns;
+        else if (root.startInterval > eIdx || root.endInterval < sIdx)
+            return 0;
+        else
+            return rangeQuery(root.left, sIdx, eIdx) + rangeQuery(root.right, sIdx, eIdx);
     }
 
+    // T(N) =
     public int update(int idx, int elt) {
         return update(root, idx, elt);
     }
 
-    private int update(Node node, int idx, int elt) {
-        if (idx >= node.startInterval || idx <= node.endInterval) {
-            return node.data;
+    public int update(Node node, int idx, int elt) {
+        if (idx >= node.startInterval && idx <= node.endInterval) {
+            if (idx == node.startInterval && idx == node.endInterval) {
+                // update
+                node.data = elt;
+                return node.data;
+            } else {
+                // backtrack
+                int leftSum = update(node.left, idx, elt);
+                int rightSum = update(node.right, idx, elt);
+                node.data = leftSum + rightSum;
+                return node.data;
+            }
         }
-
-        if (node.startInterval == node.endInterval && node.startInterval == idx) {
-            node.data = elt;
-            return node.data;
-        }
-
-        int leftAns = update(node.left, idx, elt);
-        int rightAns = update(node.right, idx, elt);
-
-        node.data = leftAns + rightAns;
         return node.data;
     }
 
+    // T(N) = O(N)
     public void display() {
-        display(root);
+        display(this.root);
     }
 
     public void display(Node root) {
-        if (root == null)
-            return;
         String str = "";
-        str = str + "Interval [" + root.startInterval + "-" + root.endInterval + "] | Data " + root.data;
+        str = str + "Interval - [" + root.startInterval + " - " + root.endInterval + "] | Data - " + root.data + " | ";
         System.out.println(str);
-        str += "\n";
-        if (root.left != null)
+        if (root.left != null) {
             display(root.left);
-        if (root.right != null)
+        }
+        if (root.right != null) {
             display(root.right);
+        }
     }
 
     public static void main(String[] args) {
+        /*
+         * 8
+         * 2 3 5 -6 12 1 4 9
+         */
         Scanner sc = new Scanner(System.in);
         int N = sc.nextInt();
         int[] arr = new int[N];
@@ -100,14 +101,9 @@ public class SegmentTree {
             arr[i] = sc.nextInt();
         SegmentTree segmentTree = new SegmentTree(arr);
         segmentTree.display();
-        System.out.println("Sum b/w range 2,6");
-        System.out.println(segmentTree.rangeQuery(2, 6));
-
-        System.out.println("Updating index 5 with value -15");
-        segmentTree.update(5, -15);
+        System.out.println(segmentTree.rangeQuery(3, 6)); // 11
+        segmentTree.update(3, -10);
         segmentTree.display();
-        System.out.println("Sum b/w range 2,6 after update");
-        System.out.println(segmentTree.rangeQuery(2, 6));
+        System.out.println(segmentTree.rangeQuery(3, 6)); // 7
     }
-
 }
