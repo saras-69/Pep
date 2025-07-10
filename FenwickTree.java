6417b610e96fa690394e89cd303859bc29dbda86
saras-69 <115482884+saras-69@users.noreply.github.com>
2025-07-10 15:42:49 +0530

    commit

--- FenwickTree.java
+++ FenwickTree.java
@@ -30,6 +30,8 @@ public class FenwickTree {
         return range(j) - range(i - 1);
     }
 
+
+
     public static void main(String[] args) {
         int[] arr = { 10, 20, 30, 40, 50, 60, 70, 80 };
         int N = arr.length;
