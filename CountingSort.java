e8b66f4b4a8de552ae16f6225445062f1402e5d8
saras-69 <115482884+saras-69@users.noreply.github.com>
2025-07-10 16:30:26 +0530

    commit

--- CountingSort.java
+++ CountingSort.java
@@ -6,29 +6,34 @@ public class CountingSort {
         int max = Arrays.stream(arr).max().getAsInt();
         int[] count = new int[max + 1];
         int[] solution = new int[N];
+
+        // Count occurrences
         for (int i = 0; i < N; i++) {
-            count[arr[i]] = ++count[arr[i]];
-            for (int c : count) {
-                System.out.println(c + " ");
-            }
+            count[arr[i]]++;
         }
-        // make prefix sum
-        for (int i = 0; i < count.length; i++) {
+
+        // Make prefix sum
+        for (int i = 1; i < count.length; i++) {
             count[i] += count[i - 1];
         }
 
+        // Build the sorted array
+        for (int i = N - 1; i >= 0; i--) {
+            solution[count[arr[i]] - 1] = arr[i];
+            count[arr[i]]--;
+        }
+
+        return solution;
     }
 
     public static void main(String[] args) {
         int[] arr = { 2, 5, 3, 0, 2, 3, 0, 3 };
-        int[] solution = new int[arr.length];
 
-        CountingSort countingSort = new countingSort(arr);
-        solution = countingSort.countSort(arr);
+        CountingSort countingSort = new CountingSort();
+        int[] solution = countingSort.countSort(arr);
+
         for (int elt : solution) {
-            System.out.println(elt + " ");
+            System.out.print(elt + " ");
         }
-
     }
-
 }
