b769f08546861a939ad0cbbeddf5d3d855d9b9f5
saras-69 <115482884+saras-69@users.noreply.github.com>
2025-07-14 16:10:32 +0530

    commit

---
+++ WeeklyTest3/Question1/tempCodeRunnerFile.java
@@ -0,0 +1,65 @@
+import java.util.*;
+
+class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int numEdges = sc.nextInt();
+
+        int[][] bg = new int[numEdges][2];
+        for (int i = 0; i < numEdges; i++) {
+            bg[i][0] = sc.nextInt();
+            bg[i][1] = sc.nextInt();
+        }
+
+        int[] result = connectedComponents(bg);
+
+        System.out.println(result[0] + " " + result[1]);
+
+    }
+
+    public static int[] connectedComponents(int[][] bg) {
+        Set<Integer> allNodes = new HashSet<>();
+        for (int[] edge : bg) {
+            allNodes.add(edge[0]);
+            allNodes.add(edge[1]);
+        }
+        Map<Integer, List<Integer>> graph = new HashMap<>();
+        for (int node : allNodes) {
+            graph.put(node, new ArrayList<>());
+        }
+        for (int[] edge : bg) {
+            int u = edge[0];
+            int v = edge[1];
+            graph.get(u).add(v);
+            graph.get(v).add(u);
+        }
+        Set<Integer> visited = new HashSet<>();
+        List<Integer> componentSizes = new ArrayList<>();
+
+        for (int node : allNodes) {
+            if (!visited.contains(node)) {
+                int componentSize = dfs(node, graph, visited);
+                if (componentSize >= 2) {
+                    componentSizes.add(componentSize);
+                }
+            }
+        }
+        int minSize = Collections.min(componentSizes);
+        int maxSize = Collections.max(componentSizes);
+
+        return new int[] { minSize, maxSize };
+    }
+
+    private static int dfs(int node, Map<Integer, List<Integer>> graph, Set<Integer> visited) {
+        visited.add(node);
+        int size = 1;
+        for (int neighbor : graph.get(node)) {
+            if (!visited.contains(neighbor)) {
+                size += dfs(neighbor, graph, visited);
+            }
+        }
+
+        return size;
+    }
+
+}
---
+++ WeeklyTest3/Question2/Main.java
@@ -0,0 +1,8 @@
+package WeeklyTest3.Question2;
+
+public class Main {
+    public static void main(String[] args) {
+
+    }
+
+}
