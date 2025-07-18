fd135d40b2b7e30379ee01f5aa4cad6853dba17a
saras-69 <115482884+saras-69@users.noreply.github.com>
2025-07-18 16:14:49 +0530

    commit

--- MCM.java
+++ MCM.java
@@ -1,26 +1,34 @@
+import java.util.Arrays;
+
 public class MCM {
-    public static int mcmRecursiveUtil(int[] arr, int i, int j) {
+    public static int mcmRecursiveUtil(int[] arr, int i, int j, int[][] dp) {
         if (i == j)
             return 0;
+
+        if (dp[i][j] != 0)
+            return dp[i][j];
+
         int min = Integer.MAX_VALUE;
         for (int k = i; k < j; k++) {
-            int ans = mcmRecursiveUtil(arr, i, k) + mcmRecursiveUtil(arr, k + 1, j) + (arr[i - 1] * arr[k] * arr[j]);
+            int ans = mcmRecursiveUtil(arr, i, k, dp) + mcmRecursiveUtil(arr, k + 1, j, dp)
+                    + (arr[i - 1] * arr[k] * arr[j]);
             if (ans < min)
                 min = ans;
         }
-        return min;
+
+        return dp[i][j] = min;
     }
 
     public static int mcmRecursive(int[] arr) {
         int n = arr.length;
-        return mcmRecursiveUtil(arr, 1, n - 1);
+        int[][] dp = new int[n][n];
+        for (int i = 0; i < n; i++)
+            Arrays.fill(dp[i], 0);
+        return mcmRecursiveUtil(arr, 1, n - 1, dp);
     }
 
     public static void main(String[] args) {
-
         int[] arr = { 2, 3, 2, 2 };
         System.out.println(mcmRecursive(arr));
-
     }
-
 }
