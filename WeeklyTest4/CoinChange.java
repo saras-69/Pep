8aee201b85e0182500505d7123dc8e2d2d121d28
saras-69 <115482884+saras-69@users.noreply.github.com>
2025-07-23 15:52:20 +0530

    commit

---
+++ WeeklyTest4/CoinChange.java
@@ -0,0 +1,28 @@
+import java.util.*;
+
+public class CoinChange {
+    static long Ways(int n, int[] c) {
+        long[] dp = new long[n + 1];
+        dp[0] = 1;
+        for (int coin : c) {
+            for (int i = coin; i <= n; i++) {
+                dp[i] += dp[i - coin];
+            }
+        }
+
+        return dp[n];
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        int m = scanner.nextInt();
+        int[] coins = new int[m];
+
+        for (int i = 0; i < m; i++) {
+            coins[i] = scanner.nextInt();
+        }
+
+        System.out.println(Ways(n, coins));
+    }
+}
--- WeeklyTest4/Main.java
+++ WeeklyTest4/Main.java
@@ -1,28 +1,5 @@
-import java.util.*;
+package WeeklyTest4;
 
 public class Main {
-    static long Ways(int n, int[] c) {
-        long[] dp = new long[n + 1];
-        dp[0] = 1;
-        for (int coin : c) {
-            for (int i = coin; i <= n; i++) {
-                dp[i] += dp[i - coin];
-            }
-        }
 
-        return dp[n];
-    }
-
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        int n = scanner.nextInt();
-        int m = scanner.nextInt();
-        int[] coins = new int[m];
-
-        for (int i = 0; i < m; i++) {
-            coins[i] = scanner.nextInt();
-        }
-
-        System.out.println(Ways(n, coins));
-    }
 }
